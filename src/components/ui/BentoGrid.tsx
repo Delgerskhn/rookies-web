702133bc8850d23efa38dcdfb689f5a2c209f3d7
Delgerskhn <57222574+Delgerskhn@users.noreply.github.com>
2024-04-09 16:35:37 +0800

    solutions section lighter bg on dark theme

--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -29,7 +29,7 @@ export const BentoGridItem = ({
     <Link
       href={href}
       className={cn(
-        'group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-transparent bg-white p-4 shadow-lg transition duration-200 hover:shadow-2xl dark:border-white/[0.2] dark:bg-slate-900 ',
+        'group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-transparent bg-white p-4 shadow-lg transition duration-200 hover:shadow-2xl dark:border-white/[0.2] dark:bg-gray-700 ',
         className,
       )}
     >
