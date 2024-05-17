dd59cd91ecd7cf66ece073e900b45cdafa8d1a37
Delgerskhn <57222574+Delgerskhn@users.noreply.github.com>
2024-05-17 15:17:49 +0800

    add posthog analytics

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -15,6 +15,7 @@ import { Locale, getDictionary } from '~/locales/getDictionary';
 const customFont = CustomFont({ subsets: ['latin'], variable: '--font-custom' });
 import { headerData } from '~/shared/data';
 import { IconBrandInstagram, IconBrandFacebook, IconBrandGithub } from '@tabler/icons-react';
+import { CSPostHogProvider } from './providers';
 
 export interface LayoutProps {
   children: React.ReactNode;
@@ -45,63 +46,65 @@ export default async function RootLayout({ children, params: { lang } }: LayoutP
         <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body className="bg-white tracking-tight text-gray-900 antialiased dark:bg-slate-900 dark:text-slate-300">
-        <Providers>
-          {/* <Announcement /> */}
-          <Header
-            headerData={{
-              links: [],
-              actions: [
-                {
-                  text: contact.title,
-                  href: '#contact',
-                  targetBlank: false,
-                  btnType: 'primary',
-                },
-              ],
-              isSticky: true,
-              showToggleTheme: true,
-              showRssFeed: false,
-              position: 'right',
-            }}
-          />
-          <main>{children}</main>
-          <Footer2
-            footerData={{
-              links: [],
-              columns: [
-                {
-                  title: contact.address.title,
-                  texts: contact.address.description,
-                },
-                {
-                  title: contact.phone.title,
-                  texts: contact.phone.description,
-                },
-                {
-                  title: contact.email.title,
-                  texts: ['info@rookies.mn'],
-                },
-              ],
-              socials: [
-                // { label: 'Twitter', icon: IconBrandTwitter, href: '#' },
-                { label: 'Instagram', icon: IconBrandInstagram, href: 'https://www.instagram.com/ai.rookies/' },
-                {
-                  label: 'Facebook',
-                  icon: IconBrandFacebook,
-                  href: 'https://www.facebook.com/profile.php?id=100092715045205',
-                },
-                // { label: 'RSS', icon: IconRss, href: '#' },
-                { label: 'Github', icon: IconBrandGithub, href: 'https://github.com/airookies' },
-              ],
-              footNote: (
-                <div className="mr-4 text-sm dark:text-slate-400">
-                  <span className="float-left mr-1.5 h-5 w-5 rounded-sm bg-[url(/favicon-32x32.png)] bg-cover md:-mt-0.5 md:h-6 md:w-6"></span>
-                  Made by AI Rookies · All rights reserved.
-                </div>
-              ),
-            }}
-          />
-        </Providers>
+        <CSPostHogProvider>
+          <Providers>
+            {/* <Announcement /> */}
+            <Header
+              headerData={{
+                links: [],
+                actions: [
+                  {
+                    text: contact.title,
+                    href: '#contact',
+                    targetBlank: false,
+                    btnType: 'primary',
+                  },
+                ],
+                isSticky: true,
+                showToggleTheme: true,
+                showRssFeed: false,
+                position: 'right',
+              }}
+            />
+            <main>{children}</main>
+            <Footer2
+              footerData={{
+                links: [],
+                columns: [
+                  {
+                    title: contact.address.title,
+                    texts: contact.address.description,
+                  },
+                  {
+                    title: contact.phone.title,
+                    texts: contact.phone.description,
+                  },
+                  {
+                    title: contact.email.title,
+                    texts: ['info@rookies.mn'],
+                  },
+                ],
+                socials: [
+                  // { label: 'Twitter', icon: IconBrandTwitter, href: '#' },
+                  { label: 'Instagram', icon: IconBrandInstagram, href: 'https://www.instagram.com/ai.rookies/' },
+                  {
+                    label: 'Facebook',
+                    icon: IconBrandFacebook,
+                    href: 'https://www.facebook.com/profile.php?id=100092715045205',
+                  },
+                  // { label: 'RSS', icon: IconRss, href: '#' },
+                  { label: 'Github', icon: IconBrandGithub, href: 'https://github.com/airookies' },
+                ],
+                footNote: (
+                  <div className="mr-4 text-sm dark:text-slate-400">
+                    <span className="float-left mr-1.5 h-5 w-5 rounded-sm bg-[url(/favicon-32x32.png)] bg-cover md:-mt-0.5 md:h-6 md:w-6"></span>
+                    Made by AI Rookies · All rights reserved.
+                  </div>
+                ),
+              }}
+            />
+          </Providers>
+        </CSPostHogProvider>
         <Analytics />
       </body>
     </html>
